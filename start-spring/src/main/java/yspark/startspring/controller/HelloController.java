b9fb98e3321d4591a729a4fca276c360bbaeb43c
ysparrk <youngseo0703@gmail.com>
2023-03-29 21:09:16 +0900

    [spring intro] : MVC

--- start-spring/src/main/java/yspark/startspring/controller/HelloController.java
+++ start-spring/src/main/java/yspark/startspring/controller/HelloController.java
@@ -3,6 +3,7 @@ package yspark.startspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller // 삽입
 public class HelloController {
@@ -11,6 +12,12 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";  // templates에서 이름이 같은 html 파일을 찾아서 렌더링 시켜준다.
+    }
 
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
+
