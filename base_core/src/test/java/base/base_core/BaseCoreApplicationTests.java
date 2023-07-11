0d51044f9f41724d6bdd47531b61d36e4b20bb73
ysparrk <youngseo0703@gmail.com>
2023-07-11 17:35:57 +0900

    setting: start

---
+++ base_core/src/test/java/base/base_core/BaseCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package base.base_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
